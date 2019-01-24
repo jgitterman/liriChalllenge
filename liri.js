2a1509c06960b4ffe81b6607e5e313e061754417
Justin Gitterman <justingitterman@gmail.com>
2019-01-23 20:48:43 -0500

    created all the necessary files

---
+++ liri.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var Spotify = require("node-spotify-api");
+var axios = require("axios")
+var fs = require("fs")
+
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
