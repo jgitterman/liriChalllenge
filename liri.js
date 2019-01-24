826d0e7b03b6bff6c4a7f1a6f2e21edecb591e35
Justin Gitterman <justingitterman@gmail.com>
2019-01-23 21:37:34 -0500

    add the concert-this feature and the bands in town api

--- liri.js
+++ liri.js
@@ -9,3 +9,24 @@ var spotify = new Spotify({
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 });
+
+
+var command = process.argv[2];
+
+
+if (command === "concert-this") {
+  const artist = process.argv.splice(3, process.argv.length).join(" ")
+
+  console.log(`\nArtist Search: ${artist}`)
+  console.log(`run the concert search for ${artist}\n`)
+
+  var query = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+  axios.get(query).then(res => {
+    console.log(`Venue: ${res.data[0].venue.name}`)
+    console.log(`Venue Location: ${res.data[0].venue.city}`)
+      // Is this what they mean by location??
+    console.log(`Date of Concert: ${res.data[0].datetime}`)
+      // How do i implement moment.js here??
+  })
+} 
